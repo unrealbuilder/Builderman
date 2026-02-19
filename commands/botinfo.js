5e1c0d9e1fb4e18dad6323469f983cda8215214e
unrealbuilder <santiagochile941@gmail.com>
2026-02-19 12:18:47 +0800

    Builderman alpha v0.0.9

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,21 +1,26 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-import pkg from '../package.json' assert { type: "json" };
+import fs from 'fs';
+
+const pkg = JSON.parse(
+  fs.readFileSync(new URL('../package.json', import.meta.url))
+);
 
 export const data = new SlashCommandBuilder()
   .setName('botinfo')
-  .setDescription('Displays information about the bot');
+  .setDescription('Displays information about the bot.');
 
-export async function execute(client, interaction) {
+export async function execute(interaction) {
+
   const embed = new EmbedBuilder()
     .setTitle('🤖 Bot Info')
     .setColor('Blue')
     .addFields(
-      { name: 'Name', value: client.user.tag, inline: true },
-      { name: 'Servers', value: `${client.guilds.cache.size}`, inline: true },
-      { name: 'Uptime', value: `<t:${Math.floor(Date.now() / 1000)}:R>`, inline: true },
-      { name: 'Version', value: pkg.version, inline: true },
+      { name: 'Name', value: interaction.client.user.tag, inline: true },
+      { name: 'Servers', value: `${interaction.client.guilds.cache.size}`, inline: true },
+      { name: 'Version', value: pkg.version, inline: true }
     )
-    .setFooter({ text: 'Made with studs | Open Source' });
+    .setFooter({ text: 'Made with studs | Open Source' })
+    .setTimestamp();
 
   await interaction.reply({ embeds: [embed] });
 }
